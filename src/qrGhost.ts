e4e608f41ea00ba7a6bd6830f56c565be75923ff
Geoffrey <dotrey@gmail.com>
2019-12-23 19:21:46 +0100

    typ into camera

--- src/qrGhost.ts
+++ src/qrGhost.ts
@@ -4,13 +4,21 @@ export default class qrGhost {
     private canvas : HTMLCanvasElement;
     private result : HTMLDivElement;
     private container : HTMLElement;
+    private video : HTMLVideoElement;
+    private videoConstraints : MediaStreamConstraints = {
+        video : true,
+        audio : false
+    }
+    private videoContainer : HTMLElement;
 
     private qr : qrWrapper;
 
     constructor() {
-        this.container = document.querySelector("main");
-        this.createCanvas();
-        this.createResult();
+        this.container = document.getElementById("main-container");
+        this.videoContainer = document.getElementById("video-container");
+        this.setupCanvas();
+        this.setupResult();
+        this.setupVideo();
         this.qr = new qrWrapper(this.displayResult.bind(this));
     }
 
@@ -35,18 +43,78 @@ export default class qrGhost {
         this.result.innerHTML = result;
     }
 
-    private createCanvas() {
-        this.canvas = document.createElement("canvas");
-        this.canvas.classList.add("image");
-        this.container.appendChild(this.canvas);
+    showVideo() {
+        if (!navigator.mediaDevices) {
+            this.log("no media device support");
+            return;
+        }
+
+        this.videoContainer.style.display = "block";
+        navigator.mediaDevices.getUserMedia(this.videoConstraints)
+            .then((stream : MediaStream) => {
+                this.log("video stream found");
+                stream.onremovetrack = (ev : MediaStreamTrackEvent) => {
+                    this.log("video stream ended");
+                }
+                this.video.srcObject = stream;
+                this.video.play();
+            })
+            .catch((error) => {
+                if (error.name === "ConstraintNotSatisfiedError") {
+                    this.log("No video stream available for specified constraints.", this.videoConstraints);
+                }else if (error.name === "PermissionDeniedError") {
+                    this.log("User permission was denied.")
+                }else{
+                    this.log("Error while accessing video stream.", error);
+                }
+            });
+    }
+
+    private adjustVideoSize() {
+        this.video.width = this.videoContainer.offsetWidth;
+        let adjust = (time : number) => {
+            console.log("offsetWidth " + this.video.offsetWidth);
+            console.log("offsetHeight " + this.video.offsetHeight);
+            console.log("width " + this.video.width);
+            if (this.video.offsetWidth !== this.video.width) {
+                console.log("-> retry");
+                window.requestAnimationFrame(adjust);
+                return;
+            }
+
+            // request an animation frame so the video can be layouted after setting the width
+            if (this.video.offsetHeight < this.video.width) {
+                // make sure the video at least fills the cut-out area
+                this.video.width = Math.ceil((this.video.width / this.video.offsetHeight) * this.video.width);
+                this.log("new width " + this.video.width);
+            }
+        };
+        window.requestAnimationFrame(adjust);
+    }
+
+    private setupCanvas() {
+        this.canvas = document.getElementById("qr-image") as HTMLCanvasElement;
         this.canvas.width = this.canvas.offsetWidth;
         this.canvas.height = this.canvas.offsetHeight;
     }
 
-    private createResult() {
-        this.result = document.createElement("div");
-        this.result.classList.add("result");
-        this.container.appendChild(this.result);
+    private setupResult() {
+        this.result = document.getElementById("qr-result") as HTMLDivElement;
+    }
+
+    private setupVideo() {
+        this.video = document.getElementById("qr-video") as HTMLVideoElement;
+        this.video.addEventListener("play", (e : Event) => {
+            this.log("playing");
+            this.adjustVideoSize();
+        });
+    }
+
+    private log(msg : string, o? : any) {
+        console.log("qr ghost >> " + msg);
+        if (o) {
+            console.log(o);
+        }
     }
 
     demoQrCode(file : string = "/assets/img/demo/qr-url.png") {
